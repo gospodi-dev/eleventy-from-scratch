0f1667b4febae032315860410379b6ddc9e94ed3
Gospodi <gospodijazzet@gmail.com>
2022-09-28 23:31:01 +0300

    составлен фрагмент для шапки сайта

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 module.exports = config => {
-  return {
+	// Set directories to pass through to the dist folder
+	config.addPassthroughCopy('./src/images/');
+	return {
 		templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+			"md",
+			"njk",
+			"html",
+			"liquid"
+		],
 
 		markdownTemplateEngine: 'njk',
 		dataTemplateEngine: 'njk',
